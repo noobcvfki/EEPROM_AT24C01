2b3d1ddade2137b0dec2c5dc85b5ccb9c72f3d68
capting <2364448632@qq.com>
2025-11-22 22:12:01 +0800

    feat(system): 添加单元测试库并集成到项目中
    - 创建新的静态库 unit 并配置源文件和头文件路径
    - 在主 CMakeLists.txt 中添加 System 子目录以包含单元测试库
    - 将 unit 库链接至主项目，依赖 elog、STM32_Drivers、softlib 和 bsp_at24c02
    - 初始化 unit.c 和 unit.h 文件结构，引入必要的头文件支持
    - 更新顶层 CMake 配置以确保新库被正确链接到目标可执行文件中

---
+++ System/unit.c
@@ -0,0 +1,7 @@
+//
+// Created by capting on 2025/11/22.
+//
+
+#include "unit.h"
+#include "iic_hal.h"
+#include "at24c02_driver.h"
---
+++ System/unit.h
@@ -0,0 +1,8 @@
+//
+// Created by capting on 2025/11/22.
+//
+
+#ifndef USER_MPUXXX_UNIT_H
+#define USER_MPUXXX_UNIT_H
+
+#endif //USER_MPUXXX_UNIT_H
