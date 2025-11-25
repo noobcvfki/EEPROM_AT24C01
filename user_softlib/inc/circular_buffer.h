6fba0d3e6bcc108762380096d21dc981c9a2d3a5
capting <2364448632@qq.com>
2025-11-25 21:07:22 +0800

    feat(buffer): 实现环形缓冲区初始化功能
    - 添加环形缓冲区结构体定义及初始化函数
    - 实现读写地址获取与更新的内部函数
    - 注册回调函数指针用于操作缓冲区地址
    - 在CMakeLists中加入源文件并链接调试模块
    - 定义DEBUG_LOG宏以支持调试日志输出

--- user_softlib/inc/circular_buffer.h
+++ user_softlib/inc/circular_buffer.h
@@ -8,6 +8,8 @@
 #include <stdint.h>
 #include "elog.h"
 
+#define DEBUG_LOG(x,...)  log_d(x,##__VA_ARGS__);
+
 typedef struct circular_buffer_struct
 {
     uint8_t* buffer;
@@ -16,11 +18,11 @@ typedef struct circular_buffer_struct
     uint8_t  wflag;
     uint8_t* (*pf_get_wbuffer_addr)(struct circular_buffer_struct*);
     uint8_t* (*pf_get_rbuffer_addr)(struct circular_buffer_struct*);
-    void     (*pf_data_write)      (struct circular_buffer_struct*);
-    void     (*pf_data_read)       (struct circular_buffer_struct*);
+    void     (*pf_data_write_addr_updata)      (struct circular_buffer_struct*);
+    void     (*pf_data_read_addr_updata)       (struct circular_buffer_struct*);
 }
-circular_buffer_struct_t;
+circular_buffer_t;
 
-void buffer_init(circular_buffer_struct_t* buffer,uint8_t size);
+void buffer_init(circular_buffer_t* buffer,uint8_t size);
 
 #endif //USER_MPUXXX_CIRCULAR_BUFFER_H
--- user_softlib/src/circular_buffer.c
+++ user_softlib/src/circular_buffer.c
@@ -1,3 +1,46 @@
 //
 // Created by capting on 2025/11/25.
 //
+
+#include "circular_buffer.h"
+#include <string.h>
+#include <stdlib.h>
+
+static uint8_t* get_wbuffer_addr(circular_buffer_t* pBuffer)
+{
+    return pBuffer->buffer + pBuffer->wflag * pBuffer->size;
+}
+
+static uint8_t* get_rbuffer_addr(circular_buffer_t* pBuffer)
+{
+    return pBuffer->buffer + pBuffer->rflag * pBuffer->size;
+}
+
+static void data_write_addr_updata(circular_buffer_t* pBuffer)
+{
+    pBuffer->wflag = (pBuffer->wflag + 1) % pBuffer->size;
+}
+
+static void data_read_addr_updata(circular_buffer_t* pBuffer)
+{
+    pBuffer->rflag = (pBuffer->rflag + 1) % pBuffer->size;
+}
+
+void buffer_init(circular_buffer_t* buffer,uint8_t size)
+{
+    if (NULL == buffer)
+    {
+        DEBUG_LOG("buffer is NULL");
+        return;
+    }
+    buffer->size = size;
+    buffer->rflag = 0;
+    buffer->wflag = 0;
+
+    buffer->buffer = (uint8_t*)malloc(size);
+
+    buffer->pf_get_rbuffer_addr = get_rbuffer_addr;
+    buffer->pf_get_wbuffer_addr = get_wbuffer_addr;
+    buffer->pf_data_read_addr_updata = data_read_addr_updata;
+    buffer->pf_data_write_addr_updata = data_write_addr_updata;
+}
