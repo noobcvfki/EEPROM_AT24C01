2a3cd1af880ad4dc8f506bbcd2e239889082b5f0
capting <2364448632@qq.com>
2025-11-25 20:48:49 +0800

    feat(buffer): 添加循环缓冲区结构体定义和初始化函数
    - 定义了循环缓冲区结构体 circular_buffer_struct_t
    - 添加了缓冲区初始化函数 buffer_init
    - 包含读写标志位和回调函数指针
    - 支持动态获取读写缓冲区地址
    - 提供数据读写操作接口
    - 引入 elog 日志库支持调试功能

--- user_softlib/inc/circular_buffer.h
+++ user_softlib/inc/circular_buffer.h
@@ -5,6 +5,22 @@
 #ifndef USER_MPUXXX_CIRCULAR_BUFFER_H
 #define USER_MPUXXX_CIRCULAR_BUFFER_H
 
+#include <stdint.h>
+#include "elog.h"
 
+typedef struct circular_buffer_struct
+{
+    uint8_t* buffer;
+    uint8_t  size;
+    uint8_t  rflag;
+    uint8_t  wflag;
+    uint8_t* (*pf_get_wbuffer_addr)(struct circular_buffer_struct*);
+    uint8_t* (*pf_get_rbuffer_addr)(struct circular_buffer_struct*);
+    void     (*pf_data_write)      (struct circular_buffer_struct*);
+    void     (*pf_data_read)       (struct circular_buffer_struct*);
+}
+circular_buffer_struct_t;
+
+void buffer_init(circular_buffer_struct_t* buffer,uint8_t size);
 
 #endif //USER_MPUXXX_CIRCULAR_BUFFER_H
